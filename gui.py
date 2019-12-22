e6c05f99a0a0acf34e917042fd34500f7e568347
Ankit Saxena <Ankit.Saxena@mail.weir>
2019-12-23 00:19:00 +0530

    Cleaning up vestigial code

--- gui.py
+++ gui.py
@@ -2,38 +2,6 @@ import PySimpleGUI as sg
 from ocr_main import main
 
 
-# sg.ChangeLookAndFeel('Dark Blue 3')
-#
-# form = sg.FlexForm('Engineering Drawings OCR', default_element_size=(40, 1))
-#
-# column1 = [[sg.Text('Orientation Threshold: ', text_color='#000000', background_color='#ffffff', justification='left',
-#                     size=(20, 1))],
-#            [sg.Slider(range=(0, 100), orientation='h', size=(20, 15), default_value=50)]]
-# column2 = [[sg.Text('Script Threshold: ', text_color='#000000', background_color='#ffffff', justification='left',
-#                     size=(20, 1))],
-#            [sg.Slider(range=(0, 100), orientation='h', size=(20, 15), default_value=50)]]
-# layout = [
-#     [sg.Text('Automated OCR for GAs', size=(30, 1), font=("Helvetica", 25))],
-#     [sg.Text('_' * 80)],
-#     [sg.Text('Choose Input Folder', size=(35, 1))],
-#     [sg.Text('Input Folder', size=(15, 1), auto_size_text=False, justification='right'),
-#      sg.InputText('Input'), sg.FolderBrowse()],
-#     [sg.Frame(layout=[
-#         [sg.Checkbox('Create OCR Text File', default=True), sg.Checkbox('Create Searchable PDF', default=False)]
-#     ],
-#         title='Additional Files', title_color='white', relief=sg.RELIEF_SUNKEN)],
-#     [sg.Frame(layout=[
-#         [sg.Column(column1, background_color='#ffffff'), sg.Column(column2, background_color='#ffffff')]
-#     ],
-#         title='Advanced Settings', title_color='white', relief=sg.RELIEF_RIDGE)],
-#     [sg.Text('_' * 80)],
-#     [sg.Submit()]
-# ]
-#
-# button, values = form.layout(layout).Read()
-# print(values)
-
-
 def ocr_gui():
     sg.ChangeLookAndFeel('Dark Blue 3')
 
