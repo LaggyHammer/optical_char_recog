71e2cb0e09e84d127255b38b1fb55ecb99633334
Ankit Saxena <Ankit.Saxena@mail.weir>
2019-12-26 11:00:25 +0530

    OCR Engine File Browsing Fix

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@ import PySimpleGUI as sg
 from ocr_main import main
 
 # application version: release.improvement.bug_fix
-app_version = '0.6.10 (Beta)'
+app_version = '0.6.11 (Beta)'
 
 
 def ocr_gui():
@@ -38,7 +38,7 @@ def ocr_gui():
             [sg.Column(column1, background_color='#ffffff'), sg.Column(column2, background_color='#ffffff')],
             [sg.Text('Choose OCR Engine :', size=(35, 1))],
             [sg.Text('OCR Engine', size=(15, 1), auto_size_text=False, justification='right'),
-             sg.InputText(r"C:\Program Files\Tesseract-OCR\tesseract.exe"), sg.FolderBrowse()]
+             sg.InputText(r"C:\Program Files\Tesseract-OCR\tesseract.exe"), sg.FileBrowse()]
         ],
             title='Advanced Settings', title_color='white', relief=sg.RELIEF_RIDGE)],
         [sg.Text('_' * 80)],
