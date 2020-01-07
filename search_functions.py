7971367b957e4660d511596460ca96baa757379d
Ankit Saxena <Ankit.Saxena@mail.weir>
2020-01-07 14:23:50 +0530

    disabled dynamic loads functionality until redesign

--- search_functions.py
+++ search_functions.py
@@ -100,26 +100,27 @@ def find_total_mass(text, key='TOTAL MASS OF'):
 
 # Dynamic Loads Function
 def find_dynamic_loads(text, key='DYNAMIC'):
-    results = []
-    key = key.lower()
-    match_list = find_near_matches(key, text, max_l_dist=2)
-    print(match_list)
-    text = text[match_list[0][0]:]
-    match_list = find_near_matches(key, text, max_l_dist=2)
-    for match in match_list:
-        search_start = match[0]
-        search_string = text[search_start:]
-        next_field_pos = search_string.find('spring')
-        search_string = search_string[: next_field_pos]
-        for m in itertools.islice(re.finditer("\d{0,5}(\.|,){0,1}(\d|o){0,3}\s{0,1}k(g|o|9)", search_string), 10):
-            # Regex: 0-5 digits + . or , (maybe) + 0-3 digits or 'o's + space (maybe) + k + g or o
-            result = m.string[m.start():m.end()]
-            # print(result)
-            results.append(result)
+    # results = []
+    # key = key.lower()
+    # match_list = find_near_matches(key, text, max_l_dist=2)
+    # print(match_list)
+    # text = text[match_list[0][0]:]
+    # match_list = find_near_matches(key, text, max_l_dist=2)
+    # for match in match_list:
+    #     search_start = match[0]
+    #     search_string = text[search_start:]
+    #     next_field_pos = search_string.find('spring')
+    #     search_string = search_string[: next_field_pos]
+    #     for m in itertools.islice(re.finditer("\d{0,5}(\.|,){0,1}(\d|o){0,3}\s{0,1}k(g|o|9)", search_string), 10):
+    #         # Regex: 0-5 digits + . or , (maybe) + 0-3 digits or 'o's + space (maybe) + k + g or o
+    #         result = m.string[m.start():m.end()]
+    #         # print(result)
+    #         results.append(result)
+    #
+    #     if bool(results):
+    #         break
+    #
+    #     results = [' ']
 
-        if bool(results):
-            break
-
-        results = [' ']
-
+    results = [' ']
     return results
