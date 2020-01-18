6cf09c05ad33c8cc2bdadc6a94ccaf2ae49d6b6a
Ank!t <ankch24@gmail.com>
2020-01-18 20:40:48 +0530

    Maintenance Commit

--- Automated_OCR.py
+++ Automated_OCR.py
@@ -19,7 +19,7 @@ def find_static_load(text, key='STATIC LOAD PER SUPPORT POINT'):
     key = key.lower()
     match_list = find_near_matches(key, text, max_l_dist=2)
     for match in match_list:
-        search_start = match[0]
+        search_start = match.start
         search_string = text[search_start:]
         for m in itertools.islice(
                 re.finditer("p.{0,2}s{0,1}\s{0,1}=\s{0,1}\d{1,5}(\.|,){0,1}(\d|o){0,3}\s{0,1}k(g|o|9)",
@@ -45,7 +45,7 @@ def find_spring_constant(text, key='SPRING CONSTANT OF'):
     key = key.lower()
     match_list = find_near_matches(key, text, max_l_dist=2)
     for match in match_list:
-        search_start = match[0]
+        search_start = match.start
         search_string = text[search_start:]
         for m in itertools.islice(re.finditer("\d{0,3}(\.|,){0,1}(\d|o){0,3}\s{0,1}k(g|o|9)\/mm", search_string), 1):
             # Regex: 0-3 digits + . or , (maybe) + 0-3 digits or 'o's + space (maybe) + k + g or o + /mm
@@ -67,7 +67,7 @@ def find_operating_speed(text, key='OPERATING SPEED'):
     key = key.lower()
     match_list = find_near_matches(key, text, max_l_dist=2)
     for match in match_list:
-        search_start = match[0]
+        search_start = match.start
         search_string = text[search_start:]
         for m in itertools.islice(
                 re.finditer("\d{1,5}\s{0,1}(r.{0,1}\s{0,1}p.{0,1}\s{0,1}m.{0,1}|r\s{0,1}\/\s{0,1}min){0,1}",
@@ -91,8 +91,9 @@ def find_total_mass(text, key='TOTAL MASS OF'):
     results = []
     key = key.lower()
     match_list = find_near_matches(key, text, max_l_dist=2)
+    print(match_list)
     for match in match_list:
-        search_start = match[0]
+        search_start = match.start
         search_string = text[search_start:]
         for m in itertools.islice(re.finditer("\d{1,5}\s{0,1}k(g|o|9)", search_string), 1):
             # Regex: 1-5 digits + space (maybe) + k + g or o
